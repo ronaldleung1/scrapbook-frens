44de86da12f4b640b81cbd975d5334066e3b400a
googol88 <ronaldfleung@gmail.com>
2021-01-23 23:10:52 -0500

    add dynamic routes

--- pages/[member].js
+++ pages/[member].js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from 'next/link';
 import styles from "../styles/Home.module.css";
 import fetch from "isomorphic-unfetch";
 
@@ -6,29 +7,29 @@ export default function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Scrapbook Frens</title>
+        <title>{props.user.profile.username} | Scrapbook Frens</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <img src={props.user.avatar} className={styles.avatar} />
+        <img src={props.user.profile.avatar} className={styles.avatar} />
         <h1 className={styles.title}>
-          Frens of <span className={styles.accent}>@{props.user.username}</span>
+          Frens of <span className={styles.accent}>@{props.user.profile.username}</span>
         </h1>
         <h2 className={styles.description}>Visit their&nbsp;
-          <a href={"https://scrapbook.hackclub.com/" + props.user.username}>Scrapbook</a>
+          <a href={"https://scrapbook.hackclub.com/" + props.user.profile.username}>Scrapbook</a>
           <span> | </span>
-          <a href={"https://hackclub.slack.com/team/" + props.user.slack}>Slack</a>
-          {(props.user.github || props.user.website) && (<span> | </span>)}
-          {props.user.github && (
-            <a href={props.user.github}>GitHub</a>
+          <a href={"https://hackclub.slack.com/team/" + props.user.profile.slack}>Slack</a>
+          {(props.user.profile.github || props.user.profile.website) && (<span> | </span>)}
+          {props.user.profile.github && (
+            <a href={props.user.profile.github}>GitHub</a>
           )}
-          {props.user.github && props.user.website && (<span> | </span>)}
-          {props.user.website && (
-            <a href={props.user.website}>Website</a>
+          {props.user.profile.github && props.user.profile.website && (<span> | </span>)}
+          {props.user.profile.website && (
+            <a href={props.user.profile.website}>Website</a>
           )}
         </h2>
         <div className={styles.grid}>
-          {props.frens.map((fren) => {
+          {props.user.webring.map((fren) => {
             return (
               <div
                 className={styles.card}
@@ -40,7 +41,7 @@ export default function Home(props) {
                     if(frn == fren.id)
                       return;
                   })*/}
-                  {fren.webring.filter(f => f == props.user.id)[0]
+                  {fren.webring.filter(f => f == props.user.profile.id)[0]
                     && <span className={styles.muted}> ∞</span>}
                 </h3>
                 <p>
@@ -68,19 +69,27 @@ export default function Home(props) {
   );
 }
 
-export async function getServerSideProps() {
-  let users = await fetch(
-    "https://scrapbook.hackclub.com/api/users/"
+export async function getServerSidePaths() {
+  const paths = ['googol'];
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getServerSideProps({ params, res }) {
+  let user = await fetch(
+    "https://scrapbook.hackclub.com/api/users/"+params.member
   ).then((r) => r.json());
-  let ringUsers = users.filter(u => u.webring.length != 0);
+  /*let ringUsers = users.filter(u => u.webring.length != 0);
   let user = ringUsers[Math.floor(Math.random() * ringUsers.length)];
   
   let frens = user.webring.map((fren) => {
     return users.filter(u => u.id == fren)[0];
-  });
+  });*/
   console.log(user);
-  console.log(frens);
+  //console.log(frens);
   return {
-    props: { user, frens },
+    props: { user },
   };
 }
